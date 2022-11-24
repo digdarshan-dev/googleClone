fb92df33e0b4d9f73f8901496b05da53bd3645bc
digdarshan-dev <ddeveloper8599@gmail.com>
2022-11-24 10:04:55 +0530

    Get data from google api

--- pages/search.js
+++ pages/search.js
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import React from 'react';
 import SearchHeader from '../components/searchHeader/SearchHeader';
 
-export default function search() {
+export default function search({ results }) {
+  console.log('result', results);
   return (
     <div>
       <Head>
@@ -13,4 +14,17 @@ export default function search() {
       {/* search result */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
+      process.env.CX_KEY
+    }&q=${context.query.term}${context.query.searchType && '&searchType=image'}`
+  ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }

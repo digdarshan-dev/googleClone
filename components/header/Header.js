229ae8ff70c990d1a17ea17db14067c306aa1218
digdarshan-dev <ddeveloper8599@gmail.com>
2022-11-24 13:42:59 +0530

    Changes made in header buttons

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,15 +1,33 @@
+import Link from 'next/link';
 import User from '../user/User';
-
+import Router, { useRouter } from 'next/router';
 export default function Header() {
+  const router = useRouter();
   return (
     <header className='flex justify-between p-5 text-sm text-gray-700'>
       <div className='flex space-x-4 items-center'>
-        <p className='links'>About</p>
-        <p className='links'>Store</p>
+        <Link href={`https://about.google/`}>
+          <p className='links'>About</p>
+        </Link>
+        <Link href={`https://store.google.com/`}>
+          <p className='links'>Store</p>
+        </Link>
       </div>
       <div className='flex space-x-4 items-center'>
-        <p className='links'>Gamil</p>
-        <p className='links'>Images</p>
+        <Link href={`https://mail.google.com/`}>
+          <p className='links'>Gamil</p>
+        </Link>
+
+        <p
+          onClick={() =>
+            Router.push(
+              `/search?term=${router.query.term || 'google'}&searchType=image`
+            )
+          }
+          className='links'
+        >
+          Images
+        </p>
         <User />
       </div>
     </header>
